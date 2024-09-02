0f94c102a9051ab419a20a2243368b8d2641a83a
CodeOfSomnath <somnathnandi368@gmail.com>
2024-09-03 01:33:36 +0530

    implementing add functions of parser

--- argparser/src/lib.rs
+++ argparser/src/lib.rs
@@ -1,6 +1,6 @@
 #![allow(unused)]
 
-use std::collections::HashMap;
+use std::{collections::HashMap, env::args};
 
 // creating a basic structure for relate functions
 /// This is the structure to store all the values
@@ -18,25 +18,61 @@ use std::collections::HashMap;
 /// println!("{}", value);
 /// ```
 struct Parser {
-    values: HashMap<String, String>,
+    strings: HashMap<String, String>,
+    ints: HashMap<String, i32>,
+    bools: HashMap<String, bool>,
+    doubles: HashMap<String, f64>,
+    longs: HashMap<String, i64>,
+    help_message: String 
 }
 
 impl Parser {
-    pub fn new() -> Self {
-        Self {
-            values: HashMap::new(),
-        }
+    pub fn new(exra_message: Option<&str>) -> Self {
+        let mut instance = Self {
+            strings: HashMap::new(),
+            ints: HashMap::new(),
+            bools: HashMap::new(),
+            doubles: HashMap::new(),
+            longs: HashMap::new(),
+            help_message: String::new()
+        };
+        let mut arg = args();
+        
+        instance.help_message += format!("{} {}", arg.nth(0).unwrap(), exra_message.unwrap()).as_str();
+
+        return instance;
     }
 
-    pub fn add_int(text: &str, help: &str, required: bool, default: i32) {}
+    pub fn add_int(&mut self, text: &str, help: &str, default: i32) {
+        self.ints.insert(text.to_string(), default);
+        let message = format!("--{}\t\t\t{}\n", text, help);
+        self.help_message += message.as_str();
 
-    pub fn add_bool(text: &str, help: &str, required: bool, default: bool) {}
+    }
 
-    pub fn add_string(text: &str, help: &str, required: bool, default: &str) {}
+    pub fn add_bool(&mut self, text: &str, help: &str, default: bool) {
+        self.bools.insert(text.to_string(), default);
+        let message = format!("--{}\t\t\t{}\n", text, help);
+        self.help_message += message.as_str();
+    }
 
-    pub fn add_double(text: &str, help: &str, required: bool, default: f64) {}
+    pub fn add_string(&mut self, text: &str, help: &str, default: &str) {
+        self.strings.insert(text.to_string(), default.to_string());
+        let message = format!("--{}\t\t\t{}\n", text, help);
+        self.help_message += message.as_str();
+    }
 
-    pub fn add_long(text: &str, help: &str, required: bool, default: i64) {}
+    pub fn add_double(&mut self, text: &str, help: &str, default: f64) {
+        self.doubles.insert(text.to_string(), default);
+        let message = format!("--{}\t\t\t{}\n", text, help);
+        self.help_message += message.as_str();
+    }
+
+    pub fn add_long(&mut self, text: &str, help: &str, default: i64) {
+        self.longs.insert(text.to_string(), default);
+        let message = format!("--{}\t\t\t{}\n", text, help);
+        self.help_message += message.as_str();
+    }
 
     pub fn parse() {}
 
