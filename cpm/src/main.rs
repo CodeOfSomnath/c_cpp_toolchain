76744675d24d68ee66132d307677c378a47021f1
CodeOfSomnath <somnathnandi368@gmail.com>
2024-09-05 15:08:20 +0530

    small changes

--- cpm/src/main.rs
+++ cpm/src/main.rs
@@ -10,6 +10,7 @@ fn make_parser() -> argparser::Parser {
     parser.add_string("new", "Create new project", "");
     parser.add_bool("lib", "Create new library project", false);
     parser.add_string("path", "Path for project", "");
+    parser.add_string("bs", "Build System type [cmake, make]", "cmake");
     parser.parse();
     return parser;
 }
---
+++ filesystem/src/lib.rs
@@ -0,0 +1,23 @@
+#![allow(unused)]
+
+pub fn create_new_file(file_path: &str) {}
+
+pub fn create_new_folder(folder_path: &str) {}
+
+struct Template;
+
+
+impl Template {
+    pub fn new_bin_template() -> Self {
+        Self
+    }
+
+    pub fn new_lib_template() -> Self {
+        Self
+    }
+
+    pub fn new_cmake_template() -> Self {
+        Self
+    }
+
+}
